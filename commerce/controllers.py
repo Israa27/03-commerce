80b3a295c666bdeee65ca528b68e5fb7be25e63a
Israa27 <israa.adnan.1997@gmail.com>
2021-11-12 02:05:14 +0300

    Edit the solution

--- commerce/controllers.py
+++ commerce/controllers.py
@@ -1,4 +1,4 @@
-from typing import Generator
+from django.core import serializers
 from ninja import Router
 from commerce.models import *
 
@@ -6,19 +6,21 @@ controller_commerce=Router()
 
 @controller_commerce.get("/products")
 def gitProducts(request):
-    vendor= [i.name for i in list(Vendor.objects.all())]
-    merchant= [i.name for i in list(Merchant.objects.all())]
-    category= [i.name for i in list(Category.objects.all())]
-    label= [i.name for i in list(Label.objects.all())]
     product_item= Product.objects.all()
     product_list = []
     for product in product_item:
+     vendor=product.vendor.name
+     merchant=product.merchant.name
+     category=product.category.name
+     label=product.label.name
+     
      product_list.append({'name' : product.name, 'description' : product.description,
       "weight" : product.weight, "width" : product.width , "height" : product.height ,
       "length" : product.length , 
        "qty" : product.qty , "cost" : product.cost , "price" : product.price ,
       "discounted_price" : product.discounted_price ,
-      "vendor" :vendor , "category" : category,"merchant" : merchant , 
+      "vendor":vendor,
+       "category" : category,"merchant" : merchant , 
        "label" :  label})
     return product_list
    
@@ -28,9 +30,10 @@ def gitProducts(request):
   
     address_list = []
     for address in address_item:     
-     city =  [i.name for i in list(City.objects.all())]
-     id=address.id
-    
+     city = address.city.name
      address_list.append({'address1' : address.address1, 'address2' : address.address2 , 'phone' : address.phone , 'city' : city })
     return address_list    
+    
+
+     
     
